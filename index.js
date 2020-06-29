0eba79cc42d9afcb32e9be45253de9478f012eff
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-06-29 16:08:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function shout(string){
   return string.toUpperCase();
-};
+}
 
 function whisper(string){
   return string.toLowerCase();
-};
+}
 
 function logShout(string){
   var upperCase = string.toUpperCase();
   console.log(upperCase);
-};
+}
 
 function logWhisper(string){
   var lowerCase = string.toLowerCase();
@@ -17,7 +17,19 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if(string.toLowerCase() === true){
+  if(string === string.toLowerCase()){
     return "I can't hear you!";
+  }
+}
+
+function sayHiToGrandma(string){
+  if(string === string.toUpperCase()){
+    return "YES INDEED!";
+  }
+}
+
+function sayHiToGrandma(string){
+  if(string === 'I love you, Grandma.'){
+    return "I love you, too."
   }
 }
